9e267ef9560eaf3e9c160b2fa7c44b8f2c1c7269
Celus8 <celus217@gmail.com>
2022-09-19 14:16:09 +0200

    Make final game

--- script.rb
+++ script.rb
@@ -1,29 +1,134 @@
 # frozen_string_literal: true
 
 class Game
-  attr_reader :board
+  attr_reader :board, :turn
 
   def initialize(board = [[], [], [], [], [], [], []])
     @board = board
+    @turn = '🔵'
+    @quit = false
+  end
+
+  def play
+    puts 'Welcome to connect four! Blue player starts'
+    play_loop
+  end
+
+  def play_loop
+    until game_over? || @quit
+      puts "It's #{@turn} player's turn!"
+      print_board
+      puts 'Enter a column number from 1 to 7'
+      player_input
+    end
+    player_turn
+    if @quit
+      puts 'You quit the game!'
+    else
+      print_board
+      puts "Congrats! #{@turn} won!"
+    end
+  end
+
+  def player_input
+    input = gets.chomp
+    input = gets.chomp until check_input(input)
+    input.to_i
+    play_round(input.to_i - 1, player_turn)
+  end
+
+  def player_turn
+    turn = @turn
+    @turn = (@turn == '🔵' ? '🔴' : '🔵')
+    turn
   end
 
   def play_round(col, player)
-    if col.negative? || col > 6
-      puts 'Invalid column!'
-      return
-    end
     @board[col].push(player)
-    print "#{@board}\n"
+  end
+
+  def check_input(input)
+    if input == 'quit'
+      @quit = true
+      return true
+    end
+    if input.to_i.zero?
+      puts 'Please input a number'
+      return false
+    end
+    if input.to_i < 1 || input.to_i > 7
+      puts 'Invalid column!'
+      return false
+    end
+    true
+  end
+
+  def print_board
+    board_to_print = Array.new(6) { Array.new(7) }
+    6.times do |i|
+      @board.each_with_index do |col, j|
+        board_to_print[5 - i][j] = col[i] || '--'
+      end
+    end
+    board_to_print.each do |row|
+      puts row.join('  ')
+    end
+  end
+
+  def transpose(board, size)
+    transpose_board = []
+    size.times do |i|
+      transpose_board << []
+      board.each do |col|
+        col[i].nil? || transpose_board[i] << col[i]
+      end
+    end
+    transpose_board
   end
 
   def game_over?
-    print "#{@board}\n"
-    over = false
-    @board.each do |col|
+    col_game_over? || row_game_over? || diag_game_over?
+  end
+
+  def col_game_over?(board = @board)
+    board.each do |col|
       col.each_cons(4) do |a|
-        over = (a.size == 4 && a.uniq.size == 1)
+        return true if a.size == 4 && a.uniq.size == 1
       end
     end
-    over
+    false
+  end
+
+  def row_game_over?
+    col_game_over?(transpose(@board, 6))
+  end
+
+  def diag_game_over?
+    diag_board = []
+    24.times { diag_board << [] }
+    6.times do |i|
+      @board.each_with_index do |col, j|
+        col[i + j].nil? || diag_board[i] << col[i + j]
+      end
+    end
+    6.times do |i|
+      @board.each_with_index do |col, j|
+        col[-i + j].nil? || diag_board[i + 6] << col[-i + j]
+      end
+    end
+    6.times do |i|
+      @board.each_with_index do |col, j|
+        col[i - j].nil? || diag_board[i + 12] << col[i - j]
+      end
+    end
+    6.times do |i|
+      @board.each_with_index do |col, j|
+        col[-i - j].nil? || diag_board[i + 18] << col[-i - j]
+      end
+    end
+    col_game_over?(diag_board)
   end
 end
+
+game = Game.new
+game.play
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -32,26 +32,144 @@ RSpec.describe Game do
       it 'raises an error' do
         col = 7
         player = 'red'
-        expect(subject).to receive(:puts).with('Invalid column!')
+        error_message = 'Invalid column!'
+        expect(subject).to receive(:puts).with(error_message)
         subject.play_round(col, player)
       end
     end
   end
 
-  describe '#game_over?' do
+  describe '#check_input' do
+    context "when input isn't a number" do
+      it 'raises an error' do
+        error_message = 'Please input a number'
+        expect(subject).to receive(:puts).with(error_message).once
+        subject.check_input('b')
+      end
+
+      it 'returns false' do
+        expect(subject.check_input('b')).to be false
+      end
+    end
+
+    context 'when input is an invalid number' do
+      it 'raises an error' do
+        error_message = 'Invalid column!'
+        expect(subject).to receive(:puts).with(error_message)
+        subject.check_input('15')
+      end
+
+      it 'returns false' do
+        expect(subject.check_input('15')).to be false
+      end
+    end
+
+    context 'when input is a valid number' do
+      it 'does not raise an error' do
+        error_message = 'Invalid column!'
+        expect(subject).not_to receive(:puts).with(error_message)
+        subject.check_input('4')
+      end
+
+      it 'returns true' do
+        expect(subject.check_input('4')).to be true
+      end
+    end
+  end
+
+  describe '#player_input' do
+    context 'when a valid number is inputted' do
+      it 'plays round' do
+        allow(subject).to receive(:gets).and_return('3')
+        expect(subject).to receive(:play_round)
+        subject.player_input
+      end
+    end
+  end
+
+  describe '#player_turn' do
+    context 'when turn is blue' do
+      it 'makes turn be red' do
+        subject.instance_variable_set(:@turn, 'blue')
+        subject.player_turn
+        expect(subject.turn).to eq('red')
+      end
+
+      it 'returns blue' do
+        subject.instance_variable_set(:@turn, 'blue')
+        expect(subject.player_turn).to eq('blue')
+      end
+    end
+  end
+
+  describe '#col_game_over?' do
     context 'when a player has a column of 4' do
       it 'returns true' do
-        win_board = [['red', 'red', 'red', 'red'], [], [], [], [], [], []]
+        win_board = [%w[red red red red], [], [], [], [], [], []]
         subject.instance_variable_set(:@board, win_board)
-        expect(subject).to be_game_over
+        expect(subject).to be_col_game_over
       end
     end
 
     context 'when no player has a column of 4' do
-      it 'returns false' do
-        lose_board = [['red', 'red', 'red'], [], [], [], [], [], []]
+      it 'returns false with 3 reds' do
+        lose_board = [%w[red red red], [], [], [], [], [], []]
         subject.instance_variable_set(:@board, lose_board)
-        expect(subject).not_to be_game_over
+        expect(subject).not_to be_col_game_over
+      end
+
+      it 'returns false with 2 reds and 2 blues' do
+        lose_board = [%w[red red blue blue], [], [], [], [], [], []]
+        subject.instance_variable_set(:@board, lose_board)
+        expect(subject).not_to be_col_game_over
+      end
+    end
+  end
+
+  describe '#row_game_over?' do
+    context 'when a player has a row of 4' do
+      it 'returns true' do
+        win_board = [['blue'], ['blue'], ['blue'], ['blue'], [], [], []]
+        subject.instance_variable_set(:@board, win_board)
+        expect(subject).to be_row_game_over
+      end
+    end
+
+    context 'when no player has a row of 4' do
+      it 'returns true with 2 blues and 2 reds' do
+        lose_board = [['blue'], ['red'], ['blue'], ['red'], [], [], []]
+        subject.instance_variable_set(:@board, lose_board)
+        expect(subject).not_to be_row_game_over
+      end
+    end
+  end
+
+  describe '#diag_game_over?' do
+    context 'when a player has a diagonal of 4' do
+      it 'returns true with an ascending diaognal' do
+        win_board = [%w[blue], %w[red blue], %w[blue red blue], %w[red red blue blue], [], [], []]
+        subject.instance_variable_set(:@board, win_board)
+        expect(subject).to be_diag_game_over
+      end
+
+      it 'returns true with a descending diagonal' do
+        win_board = [%w[red red blue blue], %w[blue red blue], %w[red blue], %w[blue], [], [], []]
+        subject.instance_variable_set(:@board, win_board)
+        expect(subject).to be_diag_game_over
+      end
+    end
+
+    context 'when no player has a diagonal of 4' do
+      it 'returns false with 2 reds and 2 blues, ascending diagonal' do
+        lose_board = [%w[blue], %w[red red], %w[blue red red], %w[red red blue blue], [], [], []]
+        subject.instance_variable_set(:@board, lose_board)
+        expect(subject).not_to be_diag_game_over
+      end
+
+      it 'returns false with 2 reds and 2 blues, descending diagonal' do
+        lose_board = [%w[red red blue red], %w[blue red red], %w[red blue], %w[blue], [], [], []]
+        subject.instance_variable_set(:@board, lose_board)
+        expect(subject).not_to be_diag_game_over
       end
     end
   end
